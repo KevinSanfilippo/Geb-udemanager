5f4f9d34e5a42630975f730901bb0865b9734511
KevinSanfilippo <kevinsanfilippo96@gmail.com>
2026-04-19 21:05:35 +0200

    Delete Gebäudemanager directory

--- Gebäudemanager/app.js
+++
@@ -1,7 +0,0 @@
-const button = document.getElementById("btngo");
-const text = document.getElementById("newtext");
-
-button.addEventListener("click", () => {
-  button.style.display = "none";   // Button verschwindet
-  text.style.display = "block";    // Text erscheint
-});
